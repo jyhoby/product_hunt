cd0316fed725206efa678343750a4c88886f701d
Unknown <382167003@qq.com>
2018-12-15 20:25:59 +0800

    增加了注册和登录功能

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.models import User
-
+from django.contrib import auth
 # Create your views here.
 
 
@@ -23,4 +23,22 @@ def signup(request):
 
 
 
+def login(request): 
+	if request.method =='GET':
+		return render(request,'login.html') 
+	elif request.method =='POST':
+		user_name = request.POST['用户名']
+		pass_word = request.POST['密码']
+		user = auth.authenticate(username=user_name,password=pass_word)
+		if user is None:
+			return render(request,'login.html',{'错误':'用户名或密码错误'}) 
+		else:
+			auth.login(request,user)
+			return redirect('主页')
+
+
+def logout(request): 
+    if request.method =='POST':
+        auth.logout(request)
+        return redirect('主页')
 
